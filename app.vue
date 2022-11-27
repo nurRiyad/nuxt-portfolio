268f4be8b458f8dda31c18d996504245a2111cf2
nurRiyad <asadnurriyad@gmail.com>
2022-11-27 15:04:17 +0600

    update meta data
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  meta: [
+    { name: "description", content: `Al Asad Nur Riyad's personal website ✨` },
+    { property: "og:image", content: "https://nurriyad.xyz/social-card.webp" },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "@nuxt_js" },
+    { name: "twitter:creator", content: "@nuxt_js" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/webp",
+      href: "/riyad.png",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage/>
+      <NuxtPage />
     </NuxtLayout>
   </div>
 </template>

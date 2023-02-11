0ffa05acc825e9f402f68adf201e7a5c09baeaf6
nurRiyad <asadnurriyad@gmail.com>
2023-02-12 00:21:49 +0600

    Update socila media card
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -16,11 +16,11 @@ useHead({
 })
 
 useServerSeoMeta({
-  title: 'Meet the Code Wizard: Crafting Magic at @AppsCode in the Bytebuilder Team',
-  ogTitle: 'Meet the Code Wizard: Crafting Magic at @AppsCode in the Bytebuilder Team',
-  description: 'Meet the Code Wizard: Crafting Magic at @AppsCode in the Bytebuilder Team',
-  ogDescription: 'Meet the Code Wizard: Crafting Magic at @AppsCode in the Bytebuilder Team',
-  ogImage: 'public/social-card.webp',
+  title: 'Al Asad Nur Riyad, Software Engineer @AppsCode in the Bytebuilder Team',
+  ogTitle: 'Al Asad Nur Riyad, Software Engineer @AppsCode in the Bytebuilder Team',
+  description: 'Hello, fellow human! I\'m a noob wizard who spends most of his day crafting code spells. When I\'m not conjuring code, you can find me summoning solutions to problems on online judges. Just don\'t ask me to cast any love spells, my magic only works on machines!',
+  ogDescription: 'Hello, fellow human! I\'m a noob wizard who spends most of his day crafting code spells. When I\'m not conjuring code, you can find me summoning solutions to problems on online judges. Just don\'t ask me to cast any love spells, my magic only works on machines!',
+  ogImage: '/social-card.webp',
   twitterCard: 'summary_large_image',
 })
 </script>

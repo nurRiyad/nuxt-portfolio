1dca7b1572f067675d7d8c9abd8e007cbd246973
nurRiyad <asadnurriyad@gmail.com>
2023-02-12 00:42:26 +0600

    Update fabicon & add robot.txt
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     '@nuxtjs/color-mode',
+    '@nuxtjs/robots',
   ],
   app: {
     head: {
@@ -11,6 +12,15 @@ export default defineNuxtConfig({
       viewport: 'width=500, initial-scale=1',
       title: 'Al Asad Nur Riyad',
       meta: [{ name: 'description', content: 'My personal site.' }],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+
+      ],
+
     },
   },
 

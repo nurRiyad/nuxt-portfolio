ae69772bc62ff1385a6b104b6ec93fe5e7b8f6a8
nurRiyad <asadnurriyad@gmail.com>
2023-02-12 02:24:37 +0600

    Update design
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="max-w-3xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 dark:text-white ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg sm:my-5">
-    <main class="max-w-none">
+  <div
+    class="max-w-3xl px-4 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 dark:text-white ring-1 ring-gray-200 dark:ring-gray-700 sm:rounded-lg sm:my-5"
+  >
+    <main>
       <slot />
     </main>
   </div>

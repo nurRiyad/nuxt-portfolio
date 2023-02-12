ae19a747b2cd2366c131b52f5ad6f019f1472ae8
Al Asad Nur Riyad <asadnurriyad@gmail.com>
2023-02-12 14:50:23 +0600

    Update the viewport value

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
+      viewport: 'width=device-width,initial-scale=1',
       title: 'Al Asad Nur Riyad',
       meta: [{ name: 'description', content: 'My personal site.' }],
       link: [

69fe0e13941f98c1022e71b33e4d4fcf25f2124f
Al Asad Nur Riyad <asadnurriyad@gmail.com>
2023-02-28 01:46:43 +0600

    Fix type
    Signed-off-by: Al Asad Nur Riyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -29,5 +29,3 @@ body {
   @apply bg-gray-50 dark:bg-gray-900;
 }
 </style>
-
-{"name":"","short_name":"","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],"theme_color":"#ffffff","background_color":"#ffffff","display":"standalone"}

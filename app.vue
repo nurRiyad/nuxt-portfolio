9ad60f59a4edb8a4b8b6ab4d3c0319d4013476b8
Al Asad Nur Riyad <asadnurriyad@gmail.com>
2023-02-28 01:45:48 +0600

    Add vite/paw
    Signed-off-by: Al Asad Nur Riyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -17,6 +17,7 @@ useServerSeoMeta({
 
 <template>
   <div>
+    <VitePwaManifest />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -28,3 +29,5 @@ body {
   @apply bg-gray-50 dark:bg-gray-900;
 }
 </style>
+
+{"name":"","short_name":"","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],"theme_color":"#ffffff","background_color":"#ffffff","display":"standalone"}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,9 @@ export default defineNuxtConfig({
     'nuxt-icon',
     '@nuxtjs/color-mode',
     '@nuxtjs/robots',
+    '@vite-pwa/nuxt',
   ],
+
   app: {
     head: {
       charset: 'utf-16',
@@ -32,4 +34,50 @@ export default defineNuxtConfig({
   },
 
   plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Al Asad Nur Riyad',
+      short_name: 'Al Asad Nur Riyad',
+      description: 'Al Asad Nur Riyad, Software Engineer @AppsCode in the Bytebuilder Team',
+      icons: [
+        {
+          src: 'favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          src: 'favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module',
+    },
+  },
+
 })

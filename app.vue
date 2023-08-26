0dc473246efd6d8a578a7e11cefd8f999204f0e7
nurRiyad <asadnurriyad@gmail.com>
2023-08-26 22:11:09 +0600

    Update site data
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -1,21 +1,58 @@
 <script setup lang="ts">
+const description = 'Hello, I am passionate software software engineer and problem solver. I am currently working at @AppsCode as Senior Software Engineer in @Bytebuilders team'
+const ogTitle = 'Al Asad Nur Riyad | Senior Software Engineer @AppsCode'
+const twitterDescription = 'Hello, I am passionate software software engineer and problem solver. I am currently working at @AppsCode as Senior Software Engineer in @Bytebuilders team'
+const image
+  = 'https://www.nurriyad.xyz/social-card.webp'
+const mySite = 'https://www.nurriyad.xyz'
 useHead({
   htmlAttrs: {
     lang: 'en',
   },
-})
-
-useSeoMeta({
-  title: 'Riyad',
-  ogTitle: 'Al Asad Nur Riyad | Senior Software Engineer @AppsCode',
-  description: 'Hello, I am passionate software software engineer and problem solver. I am currently working at @AppsCode as Senior Software Engineer in @Bytebuilders team',
-  ogDescription: 'Hello, I am passionate software software engineer and problem solver. I am currently working at @AppsCode as Senior Software Engineer in @Bytebuilders team',
-  ogImage: 'https://www.nurriyad.xyz/social-card.webp',
-  ogUrl: 'https://www.nurriyad.xyz',
-  ogType: 'website',
-  twitterCard: 'summary_large_image',
-  twitterTitle: 'Al Asad Nur Riyad | Senior Software Engineer @AppsCode',
-  twitterDescription: 'Hello, I am passionate software software engineer and problem solver. I am currently working at @AppsCode as Senior Software Engineer in @Bytebuilders team',
+  meta: [
+    {
+      name: 'description',
+      content: description,
+    },
+    // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+    { property: 'og:site_name', content: mySite },
+    { property: 'og:type', content: 'website' },
+    {
+      property: 'og:url',
+      content: mySite,
+    },
+    {
+      property: 'og:title',
+      content: ogTitle,
+    },
+    {
+      property: 'og:description',
+      content: description,
+    },
+    {
+      property: 'og:image',
+      content: image,
+    },
+    // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+    { name: 'twitter:site', content: '@debs_obrien' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      name: 'twitter:url',
+      content: mySite,
+    },
+    {
+      name: 'twitter:title',
+      content: ogTitle,
+    },
+    {
+      name: 'twitter:description',
+      content: twitterDescription,
+    },
+    {
+      name: 'twitter:image',
+      content: image,
+    },
+  ],
 })
 </script>
 

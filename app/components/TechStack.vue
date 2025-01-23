49a8cf42143ce7fdd1148f7437da01db97796b20
Md Mohin Uddin <45921824+mohin7@users.noreply.github.com>
2025-01-23 16:41:56 +0600

    Fix tech stack grid design issues (#17)
    * Fig grid issue
    
    Signed-off-by: mohin7 <mohin@appscode.com>
    
    * Fig grid issue
    
    Signed-off-by: mohin7 <mohin@appscode.com>
    
    * Removr comment
    
    Signed-off-by: mohin7 <mohin@appscode.com>
    
    ---------
    
    Signed-off-by: mohin7 <mohin@appscode.com>

--- app/components/TechStack.vue
+++ app/components/TechStack.vue
@@ -9,65 +9,65 @@ const colorMode = useColorMode()
     </h1>
     <div class="overflow-hidden border dark:border-gray-700 shadow rounded-xl">
       <div class="ease-out transition-transform px-5 py-3">
-        <div class="flex flex-wrap pb-4 gap-2 justify-start items-baseline ">
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700  rounded-md">
+        <div class="grid gap-2 grid-cols-[repeat(auto-fit,_minmax(5rem,_1fr))]">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:javascript" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:typescript-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:c-plusplus" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:nodejs-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="simple-icons:express" :color="colorMode" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:nestjs" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:vue" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:nuxt-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:react" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:nextjs-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:vitejs" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:pinia" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:react-query-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:cypress-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:cloudflare-workers-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:aws-ec2" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:linux-tux" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="simple-icons:macos" :color="colorMode" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:git-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
-          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md">
+          <div class="border dark:border-gray-600 p-2 border-gray-200 bg-gray-100 dark:bg-gray-700 rounded-md flex items-center justify-center">
             <Icon name="logos:docker-icon" class="hover:scale-[1.1] ease-out transition-transform" size="35" />
           </div>
         </div>

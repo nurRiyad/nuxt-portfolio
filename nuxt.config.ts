8a7c67d6a2421c1edb4e863da338f3db33fa98f8
nurRiyad <nur.riyad@fieldnation.com>
2025-12-04 11:28:57 +0600

    Add Google Fonts preconnect and update main CSS for font family

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,9 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;600&display=swap' },
       ],
 
     },
